1d8c38fe18ac7482b5f6ce5ecd81661b7133dd3e
Павел <psurkoff@mail.ru>
2021-03-07 12:16:44 +0700

    Ссылки навигации переключаются

--- js/catalog.js
+++ js/catalog.js
@@ -1,6 +1,11 @@
-let sortLinks = document.querySelectorAll('.crumbs-item__link');
 function rad(element) {
 	document.querySelector('.link_active')
 	.classList.remove('link_active');
 	element.classList.add('link_active');
+}
+
+function redUnder(element) {
+	document.querySelector('.navigation__link_active')
+	.classList.remove('navigation__link_active');
+	element.classList.add('navigation__link_active');
 }
